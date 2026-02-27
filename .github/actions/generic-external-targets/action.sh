4e7515c3b373003b872d75de0fed2dfa85808b8b
Alice Mikityanska <alice@isovalent.com>
2026-02-27 18:10:13 +0200

    .github: Get IPs of generic fake external targets from nodes
    Instead of getting IPs of nginx pods after they have started, changing
    the certificate, replacing the ConfigMap and reloading nginx, those IPs
    can be learned from external nodes' InternalIPs in advance, saving all
    these steps.
    
    Signed-off-by: Alice Mikityanska <alice@isovalent.com>

--- .github/actions/generic-external-targets/action.sh
+++ .github/actions/generic-external-targets/action.sh
@@ -26,6 +26,10 @@ openssl req -newkey rsa:2048 -nodes \
     -subj "/CN=$TARGETNAME" \
     -out external-service.cilium.req.pem
 
+# Figure out the addresses of the external nodes.
+IP4TARGET="$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')"
+IP4OTHERTARGET="$(kubectl get nodes -o jsonpath='{.items[1].status.addresses[?(@.type=="InternalIP")].address}')"
+
 # Create a config file to tell openssl how to turn the signing request into a certificate
 # Make sure the key usage is such that we can use the cert for HTTPS traffic.
 # Also make sure both domain names are in the subjectAltName so the certificate works for
@@ -34,7 +38,7 @@ cat > v3.ext << EOF
 subjectKeyIdentifier   = hash
 authorityKeyIdentifier = keyid:always,issuer:always
 keyUsage               = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign
-subjectAltName         = DNS:$OTHERTARGETNAME, DNS:$TARGETNAME, DNS:fake.external.first.target, DNS:fake.external.second.target
+subjectAltName         = DNS:$OTHERTARGETNAME, DNS:$TARGETNAME, DNS:fake.external.first.target, DNS:fake.external.second.target, IP:$IP4TARGET, IP:$IP4OTHERTARGET
 EOF
 
 # Turn the certificate signing request into a certificate, signed by our CA
@@ -63,62 +67,7 @@ envsubst '$NGINX_CERT_BASE64 $NGINX_KEY_BASE64 $EXTERNAL_NODE' < "$(dirname "$0"
 kubectl -n external rollout status daemonset nginx --timeout 60s
 kubectl -n external-other rollout status daemonset nginx --timeout 60s
 
-# Save nginx ConfigMap versions to track when they are updated.
-NGINX1_CONFIGMAP_VERSION="$(kubectl -n external exec "$(kubectl -n external get pods -l app=nginx -o name)" -- readlink /etc/nginx/ssl/..data)"
-NGINX2_CONFIGMAP_VERSION="$(kubectl -n external-other exec "$(kubectl -n external-other get pods -l app=nginx -o name)" -- readlink /etc/nginx/ssl/..data)"
-
-# Figure out the addresses of the newly deployed pods.
-IP4TARGET="$(kubectl -n external get pods -l app=nginx -o json | jq -r '.items[].status.hostIP')"
-IP4OTHERTARGET="$(kubectl -n external-other get pods -l app=nginx -o json | jq -r '.items[].status.hostIP')"
 echo "ipv4_external_target=$IP4TARGET" >> $GITHUB_OUTPUT
 echo "ipv4_other_external_target=$IP4OTHERTARGET" >> $GITHUB_OUTPUT
 echo "external_target_name=$TARGETNAME" >> $GITHUB_OUTPUT
 echo "other_external_target_name=$OTHERTARGETNAME" >> $GITHUB_OUTPUT
-
-# Add IP addresses to subjectAltName in the certificate.
-cat > v3.ext << EOF
-subjectKeyIdentifier   = hash
-authorityKeyIdentifier = keyid:always,issuer:always
-keyUsage               = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign
-subjectAltName         = DNS:$OTHERTARGETNAME, DNS:$TARGETNAME, DNS:fake.external.first.target, DNS:fake.external.second.target, IP:$IP4TARGET, IP:$IP4OTHERTARGET
-EOF
-
-# Turn the certificate signing request into a certificate, signed by our CA
-openssl x509 -req -days 365 -set_serial 01 \
-    -in external-service.cilium.req.pem \
-    -out external-service.cilium.crt \
-    -extfile v3.ext \
-    -CA ca-cert.pem \
-    -CAkey ca-key.pem
-
-# Update the ConfigMap with the new certificate.
-NGINX_CERT_BASE64="$(base64 -w0 external-service.cilium.crt)" \
-NGINX_KEY_BASE64="$(base64 -w0 external-service.cilium.key)" \
-EXTERNAL_NODE="${NODES_WITHOUT_CILIUM[0]}" \
-envsubst '$NGINX_CERT_BASE64 $NGINX_KEY_BASE64 $EXTERNAL_NODE' < "$(dirname "$0")/nginx-external.yaml" | kubectl -n external apply -f -
-
-NGINX_CERT_BASE64="$(base64 -w0 external-service.cilium.crt)" \
-NGINX_KEY_BASE64="$(base64 -w0 external-service.cilium.key)" \
-EXTERNAL_NODE="${NODES_WITHOUT_CILIUM[1]}" \
-envsubst '$NGINX_CERT_BASE64 $NGINX_KEY_BASE64 $EXTERNAL_NODE' < "$(dirname "$0")/nginx-external.yaml" | kubectl -n external-other apply -f -
-
-# Wait until the ConfigMap gets updated. Timeout after 10 minutes.
-CONFIGMAP_UPDATED=
-for i in $(seq 1 60); do
-	NGINX1_CONFIGMAP_VERSION_NEW="$(kubectl -n external exec "$(kubectl -n external get pods -l app=nginx -o name)" -- readlink /etc/nginx/ssl/..data)"
-	NGINX2_CONFIGMAP_VERSION_NEW="$(kubectl -n external-other exec "$(kubectl -n external-other get pods -l app=nginx -o name)" -- readlink /etc/nginx/ssl/..data)"
-	if [ "$NGINX1_CONFIGMAP_VERSION" != "$NGINX1_CONFIGMAP_VERSION_NEW" ] && [ "$NGINX2_CONFIGMAP_VERSION" != "$NGINX2_CONFIGMAP_VERSION_NEW" ]; then
-		CONFIGMAP_UPDATED=1
-		break
-	fi
-	echo 'Waiting until nginx ConfigMap is updated...'
-	sleep 10
-done
-if [ -z "$CONFIGMAP_UPDATED" ]; then
-	echo 'Timeout waiting for nginx ConfigMap update.'
-	exit 1
-fi
-
-# Reload nginx after updating the ConfigMap.
-kubectl -n external exec "$(kubectl -n external get pods -l app=nginx -o name)" -- nginx -s reload
-kubectl -n external-other exec "$(kubectl -n external-other get pods -l app=nginx -o name)" -- nginx -s reload
